2369322981506aa1de618c1fc948a32ee3948aba
潘俊杰 <panjunjie01@bilibili.com>
2022-01-18 15:31:17 +0800

    add sth

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,21 +1,16 @@
 package main
 
 import (
-	"flag"
 	"socks5/filter"
 	"socks5/server"
 )
 
-var (
-	name     = flag.String("user_name", "socks5", "socks5 proxy username")
-	password = flag.String("password", "20220101", "socks5 proxy password")
-)
-
 func main() {
-	serve := server.New(server.BasicAuth{
-		Username: *name,
-		Password: *password,
-	}, filter.NoFilter{})
+	flt, err := filter.NewHttpFilter(":80", "icy4ever")
+	if err != nil {
+		panic(err)
+	}
+	serve := server.New(server.NoAuth{}, flt)
 	if err := serve.ListenAndServe("tcp", ":1080"); err != nil {
 		panic(err)
 	}
